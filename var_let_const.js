3071ffa567cbd6626d1d7a31688962163ea6bd02
ayon20 <21101032@uap-bd.edu>
2023-12-23 14:52:51 +0600

    ver_let_const.js

---
+++ var_let_const.js
@@ -0,0 +1,7 @@
+var a = "first one"; //variable can be re-decleared  & updated. A global scope variable 
+let b ="first onec let"; //variable cannot be re-declared but can be updated . a block scope variable
+b = "updated one "
+const c ="first one copy copy"; // variable cannot be re-declared or updated . A block scope variable . 
+console.log(a);
+console.log(b);
+console.log(c);
